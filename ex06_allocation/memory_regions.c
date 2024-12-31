bff0ba4d517d770659400c4a4312fcbf57791f2b
Jay Brockman <jbb@nd.edu>
2024-12-31 12:41:53 -0500

    Updated ex06_allocation

--- ex06_allocation/memory_regions.c
+++ ex06_allocation/memory_regions.c
@@ -3,27 +3,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double G = 3.14;
+double GLOBAL = 3.14;
 
 int main(int argc, char *argv[]) {
-    int        x   = 1;
-    int        a[] = {4, 6, 6, 3, 7};
-    char      *s   = "string";
-    int       *p   = malloc(10 * sizeof(int));
-    static int t   = 2;
+    int        int_scalar   = 25;
+    int        int_array[]  = {100, 101, 102, 103};
+    char      *str_const    = "hello";
+    int       *malloc_array = malloc(10 * sizeof(int));
+    static int static_int   = 2;
 
-    puts("----------------stack----------------");
-    printf("   a: Address: %p, Size: %lu\n",  &a, sizeof( a));  // stack
-    printf("  &p: Address: %p, Size: %lu\n",  &p, sizeof(&p));  // stack
-    printf("  &s: Address: %p, Size: %lu\n",  &s, sizeof(&s));  // stack
-    printf("   x: Address: %p, Size: %lu\n",  &x, sizeof( x));  // stack
-    puts("----------------heap----------------");
-    printf("   p: Address: %p, Size: %lu\n",   p, sizeof( p)); // heap
-    puts("----------------data----------------");
-    printf("   t: Address: %p, Size: %lu\n",  &t, sizeof( t));   // data
-    printf("   G: Address: %p, Size: %lu\n",  &G, sizeof( G));  // data
-    printf("   s: Address: %p, Size: %lu\n",   s, sizeof( s));  // data
-    printf("  *s: Address: %p, Size: %lu\n", &*s, sizeof(*s));  // data (same as s)
-    puts("----------------code----------------");
-    printf("main: Address: %p, Size: %lu\n", &main, sizeof(main));  // code
+    puts("---------------------------stack----------------------------");
+    printf("    int_array: %p,    *int_array: %d\n",   int_array, *int_array);
+    printf("&malloc_array: %p,  malloc_array: %p\n",  &malloc_array,  malloc_array);
+    printf("   &str_const: %p,     str_const: %p\n",  &str_const,  str_const);
+    printf("  &int_scalar: %p,             x: %d\n",  &int_scalar,  int_scalar);
+    puts("----------------------------heap----------------------------");
+    printf(" malloc_array: %p, *malloc_array: %d\n",   malloc_array, *malloc_array);
+    puts("----------------------------data----------------------------");
+    printf("  &static_int: %p,    static_int: %d\n", &static_int,  static_int);
+    printf("      &GLOBAL: %p,        GLOBAL: %f\n", &GLOBAL,  GLOBAL);
+    printf("    str_const: %p,    *str_const: %c\n",  str_const, *str_const);
+    puts("----------------------------code----------------------------");
+    printf("         main: %p,         *main: 0x%x\n", main, (unsigned char)(*main));
 }
